6dc9db918847943f38dfeacd17ad2a4b68304f36
MhL5 <sapumr@gmail.com>
2026-02-13 02:41:50 +0330

    refactor: remove unused Tanstack Devtools and related dependencies; clean up Providers component

--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -1,18 +1,14 @@
 import { Toaster } from "@/components/ui/sonner";
-import { isDev } from "@/utils/checks";
 import { NextIntlClientProvider } from "next-intl";
 import type { PropsWithChildren } from "react";
 
 import DirectionProvider from "./DirectionProvider";
-import { TanstackFormDevtoolDynamic } from "./TanstackDevtool";
 import { ThemeProvider } from "./ThemeProvider";
 
 export default function Providers({ children }: PropsWithChildren) {
   return (
     <DirectionProvider>
       <ThemeProvider>
-        {isDev() && <TanstackFormDevtoolDynamic />}
-
         <NextIntlClientProvider>{children}</NextIntlClientProvider>
         <Toaster richColors closeButton />
       </ThemeProvider>
